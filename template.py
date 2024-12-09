7118062524259940254002c5ed36364dfe7dfaac
Mark Snyder <mark.clifford.snyder@gmail.com>
2024-12-09 01:13:32 -0500

    adding template from 2023 to this repo

---
+++ template.py
@@ -0,0 +1,18 @@
+from argparse import ArgumentParser
+
+def main(data):
+    pass
+
+def read_input(input_file):
+    data = []
+    with open(input_file, 'r') as input:
+        for line in input:
+            data.append(line.strip())
+    return data
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('input_file', nargs='?', default='input.txt')
+    args = parser.parse_args()
+    data = read_input(args.input_file)
+    main(data)

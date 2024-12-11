84abea69eb4b3509ce3473469c510354a0a8efc4
Mark Snyder <mark.clifford.snyder@gmail.com>
2024-12-11 00:20:53 -0500

    actually star 2 was pretty easy

--- Day11/pebbles.py
+++ Day11/pebbles.py
@@ -1,22 +1,24 @@
 from argparse import ArgumentParser
+from functools import cache
+
+@cache
+def num_spawned(number, blinks):
+    if not blinks:
+        return 1
+    else:
+        if number == 0:
+            return num_spawned(1, blinks-1)
+        elif not (len(str(number)) % 2):
+            str_no = str(number)
+            return num_spawned(int(str_no[:len(str_no)//2]), blinks-1) + num_spawned(int(str_no[len(str_no)//2:]), blinks-1)
+        else:
+            return num_spawned(number*2024, blinks-1)
 
 def main(data):
     numbers = [int(no) for no in data[0].split()]
 
-    for _ in range(25):
-        next_numbers = []
-        for number in numbers:
-            if number == 0:
-                next_numbers.append(1)
-            elif not (len(str(number)) % 2):
-                str_no = str(number)
-                next_numbers.append(int(str_no[:len(str_no)//2]))
-                next_numbers.append(int(str_no[len(str_no)//2:]))
-            else:
-                next_numbers.append(number * 2024)
-        numbers = next_numbers
-
-    print(len(numbers))
+    print(sum(num_spawned(number, 25) for number in numbers))
+    print(sum(num_spawned(number, 75) for number in numbers))
 
 def read_input(input_file):
     data = []

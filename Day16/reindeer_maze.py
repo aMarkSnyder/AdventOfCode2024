f70a118fb5eabba1a7be25ffe345736591a1eb46
Mark Snyder <mark.clifford.snyder@gmail.com>
2024-12-16 02:42:00 -0500

    realized I needed to add a third dimension to distances graph to efficiently find all shortest paths

--- Day16/reindeer_maze.py
+++ Day16/reindeer_maze.py
@@ -21,60 +21,63 @@ class ReindeerState:
         left_step = DIRS[(self.dir-1) % len(DIRS)]
         left = (self.loc[0]+left_step[0], self.loc[1]+left_step[1])
         if maze[left] == '.':
-            options.append(ReindeerState(self.cost+1001, left, (self.dir-1) % len(DIRS)))
+            options.append(ReindeerState(self.cost+1000, self.loc, (self.dir-1) % len(DIRS)))
 
         right_step = DIRS[(self.dir+1) % len(DIRS)]
         right = (self.loc[0]+right_step[0], self.loc[1]+right_step[1])
         if maze[right] == '.':
-            options.append(ReindeerState(self.cost+1001, right, (self.dir+1) % len(DIRS)))
+            options.append(ReindeerState(self.cost+1000, self.loc, (self.dir+1) % len(DIRS)))
 
         return options
-    
-def count_visits(maze, start, goal, target_score, path_so_far, visits_by_optimal=defaultdict(int)):
-    if start.cost > target_score:
+
+def count_visits(maze, start, goal, distances, path_so_far, visits_by_optimal=defaultdict(int)):
+    goals = [(goal[0], goal[1], dir) for dir in range(len(DIRS))]
+    if start.cost > min(distances[g] for g in goals):
         return
-    if start.cost == target_score and start.loc == goal:
+    if start.cost == min(distances[g] for g in goals) and start.loc == goal:
         for loc in path_so_far:
             visits_by_optimal[loc] += 1
         return
-    
+
     options = start.get_options(maze)
     for option in options:
-        count_visits(maze, option, goal, target_score, path_so_far + (option.loc,), visits_by_optimal)
+        if distances[option.loc + (option.dir,)] > distances[start.loc + (start.dir,)]:
+            count_visits(maze, option, goal, distances, path_so_far + (option.loc,), visits_by_optimal)
 
 def main(data):
     maze = {}
     distance = {}
     for row_idx, row in enumerate(data):
         for col_idx, val in enumerate(row):
+            for dir in range(len(DIRS)):
+                distance[(row_idx, col_idx, dir)] = np.inf
             if val == 'S':
                 start = ReindeerState(0, (row_idx, col_idx), 0)
-                distance[(row_idx, col_idx)] = 0
+                distance[(row_idx, col_idx, 0)] = 0
                 maze[(row_idx, col_idx)] = '.'
             elif val == 'E':
                 goal = (row_idx, col_idx)
-                distance[(row_idx, col_idx)] = np.inf
                 maze[(row_idx, col_idx)] = '.'
             else:
-                distance[(row_idx, col_idx)] = np.inf
                 maze[(row_idx, col_idx)] = val
 
+    # Star 1
+    goals = [(goal[0], goal[1], dir) for dir in range(len(DIRS))]
     queue = [start]
     while queue:
         curr = heappop(queue)
-        if curr.cost > distance[goal]:
+        if curr.cost > min(distance[g] for g in goals):
             break
         options = curr.get_options(maze)
         for option in options:
-            if option.cost < distance[option.loc]:
-                distance[option.loc] = option.cost
+            if option.cost < distance[option.loc + (option.dir,)]:
+                distance[option.loc + (option.dir,)] = option.cost
                 heappush(queue, option)
-    
-    print(distance[goal])
+    print(min(distance[g] for g in goals))
 
-    shortest_distance = distance[goal]
+    # Star 2
     visits_by_optimal = defaultdict(int)
-    count_visits(maze, start, goal, shortest_distance, (start.loc,), visits_by_optimal)
+    count_visits(maze, start, goal, distance, (start.loc,), visits_by_optimal)
     print(len(visits_by_optimal))
 
 def read_input(input_file):
